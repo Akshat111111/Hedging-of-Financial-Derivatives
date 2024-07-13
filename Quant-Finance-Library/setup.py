89ad35a86d07ccf61f471dff842b14d1ce4bf001
ap550083@gmail.com <ap550083@gmail.com>
2024-07-13 11:53:49 +0530

    Reinforcement Learning Model for Dynamic Hedging Strategies

--- Quant-Finance-Library/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='iquant',
-    version='1.01',
-    description='A simple Python library for quantitative finance.',
-    author='Apoorv Yadav',
-    packages=find_packages(),
-)
