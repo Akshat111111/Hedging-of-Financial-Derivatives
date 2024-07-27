561dedb543f3320a8521e758e86599053cc2c6fd
~Chiluka Akshitha <120377576+AKSHITHA-CHILUKA@users.noreply.github.com>
2024-07-27 15:47:44 +0530

    Create visualize_data.py

---
+++ Hedging with Real Estate/visualize_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_data(df):
+    sns.pairplot(df)
+    plt.show()
+
+if __name__ == "__main__":
+    df = pd.read_csv('path/to/data.csv')
+    plot_data(df)
